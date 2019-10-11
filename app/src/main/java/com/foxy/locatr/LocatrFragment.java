548c2440a7da22683220a98e61a4acbaf68617d0
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-11 16:28:23 +0800

    настройка googleClient'a

--- app/src/main/java/com/foxy/locatr/LocatrFragment.java
+++ app/src/main/java/com/foxy/locatr/LocatrFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,8 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 public class LocatrFragment extends Fragment {
     private ImageView imageView;
+    private GoogleApiClient client;
 
     public static LocatrFragment newInstance() {
         return new LocatrFragment();
@@ -23,6 +28,21 @@ public class LocatrFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);          // подключение меню
+
+        client = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        // перерисовка панели инструментов при подключении
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                    }
+                })
+                .build();
     }
 
     @Nullable
@@ -34,8 +54,26 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        client.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        client.disconnect();
+    }
+
+    @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_locatr, menu);
+
+        // активация кнопки поиска, если клиент подключен
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(client.isConnected());
     }
 }
