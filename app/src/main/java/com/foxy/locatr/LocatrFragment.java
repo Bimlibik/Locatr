8a744d7014e4d4cc13548faaea94e17103701c26
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-12 16:20:47 +0800

    *

--- app/src/main/java/com/foxy/locatr/LocatrFragment.java
+++ app/src/main/java/com/foxy/locatr/LocatrFragment.java
@@ -8,14 +8,9 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
